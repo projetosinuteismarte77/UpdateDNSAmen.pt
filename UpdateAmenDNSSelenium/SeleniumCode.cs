06d879d91d9f38a751ba11170dc06b384972a6b6
marto <martinho.tavares@live.com.pt>
2024-04-10 23:06:48 +0100

    t

--- UpdateAmenDNSSelenium/SeleniumCode.cs
+++ UpdateAmenDNSSelenium/SeleniumCode.cs
@@ -18,8 +18,7 @@ namespace UpdateAmenDNSSelenium
             var options = new FirefoxOptions();
             if(headless)
                 options.AddArgument("--headless=new");
-            var service = FirefoxDriverService.CreateDefaultService("/home/pi/noderedstuff/update_godaddy/UpdateDNSAmen.pt/geckodriver");
-            var driver = new FirefoxDriver(service);//new ChromeDriver(options);
+            var driver = new FirefoxDriver("/home/pi/noderedstuff/update_godaddy/UpdateDNSAmen.pt/geckodriver");//new ChromeDriver(options);
             Console.WriteLine(driver.Manage().Window.Size.ToString());
             var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
             var initialURL = "https://controlpanel.amen.pt/welcome.html";
