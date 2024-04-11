f2c23886dcfe7813a1c7ec9764fd25745086af5e
marto <martinho.tavares@live.com.pt>
2024-04-11 23:28:48 +0100

    Update SeleniumCode.cs

--- UpdateAmenDNSSelenium/SeleniumCode.cs
+++ UpdateAmenDNSSelenium/SeleniumCode.cs
@@ -16,7 +16,7 @@ namespace UpdateAmenDNSSelenium
 
         public static string findCommand(string whatToFind)
         {
-            ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "find", Arguments = "/snap/firefox -name "+whatToFind, RedirectStandardInput = true };
+            ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "find", Arguments = "/snap/firefox -name "+whatToFind };
             Process proc = new Process() { StartInfo = startInfo, };
             proc.Start();
             proc.WaitForExit();
