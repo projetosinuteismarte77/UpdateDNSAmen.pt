2561da5318880daa2c96ab5722edacb13086cd5f
marto <martinho.tavares@live.com.pt>
2024-04-12 11:18:32 +0100

    console log color minha vermelha

--- UpdateAmenDNSSelenium/SeleniumCode.cs
+++ UpdateAmenDNSSelenium/SeleniumCode.cs
@@ -24,30 +24,40 @@ namespace UpdateAmenDNSSelenium
             return output.Split("\n").Last();
         }
 
+        public static void Log(string log)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("[MARTINHO] " + log);
+            Console.ResetColor();
+        }
+
 		static public void UpdateDNS(bool headless, string user, string password)
 		{
+            
             var options = new FirefoxOptions();
             if(headless)
                 options.AddArgument("-headless");
+            options.AddArgument("-log-level=fatal");
             var geckodriverPath = OperatingSystem.IsMacOS() ? "/Users/marte/Documents/RandomProjeots/UpdateAmenDNSSelenium/UpdateAmenDNSSelenium/geckodriver" : "/snap/firefox/4038/usr/lib/firefox/geckodriver";//findCommand("geckodriver");//"/home/pi/noderedstuff/update_godaddy/UpdateDNSAmen.pt/geckodriver";
             if (OperatingSystem.IsLinux())
                 options.BinaryLocation = "/snap/firefox/4038/usr/lib/firefox/firefox";//findCommand("firefox");
             options.LogLevel = FirefoxDriverLogLevel.Fatal;
             options.SetLoggingPreference(LogType.Driver, LogLevel.Off);
             var driver = new FirefoxDriver(geckodriverPath, options);//new ChromeDriver(options);
-            Console.WriteLine(driver.Manage().Window.Size.ToString());
+            Log(driver.Manage().Window.Size.ToString());
             var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
             var initialURL = "https://controlpanel.amen.pt/welcome.html";
             driver.Navigate().GoToUrl(initialURL);
             wait.Until(ExpectedConditions.ElementExists(By.CssSelector(".iubenda-cs-reject-btn.iubenda-cs-btn-primary")));
             driver.FindElement(By.CssSelector(".iubenda-cs-reject-btn.iubenda-cs-btn-primary")).Click();
-            Console.WriteLine("Fazer Login");
+            Log("Fazer Login");
             int count = 0;
             Thread.Sleep(15000);
             while (driver.Url == initialURL)
             {
                 driver.Navigate().Refresh();
-                Console.WriteLine("Vou tentar count " + (count++) + " " + driver.Url);
+                Log("Vou tentar count " + (count++) + " " + driver.Url);
                 var divInputs = driver.FindElements(By.ClassName("standard-login-module"));
                 if (divInputs.Count == 0)
                     continue;
@@ -57,16 +67,16 @@ namespace UpdateAmenDNSSelenium
                 inputs[0].Clear(); inputs[0].SendKeys(user);
                 if (headless) Thread.Sleep(300);
                 inputs[1].Clear(); inputs[1].SendKeys(password);
-                if (headless) Thread.Sleep(800);
+                if (headless) Thread.Sleep(2000);
                 driver.FindElements(By.TagName("button")).Where(elem => elem.GetAttribute("type") == "submit").First().Click();
                 Thread.Sleep(3000);
             }
-            Console.WriteLine("Login Efetuado");
+            Log("Login Efetuado");
             driver.FindElements(By.TagName("a")).Where(elem => elem.GetAttribute("href") != null && elem.GetAttribute("href") == "https://controlpanel.amen.pt/firstLevel/view.html?domain=martinho.pt").First().Click();
             wait.Until(ExpectedConditions.ElementExists(By.Id("webapp_domain")));
             if (headless) Thread.Sleep(300);
             driver.FindElement(By.Id("webapp_domain")).FindElement(By.TagName("a")).Click();
-            Console.WriteLine("Selecionei o meu dominio");
+            Log("Selecionei o meu dominio");
             Thread.Sleep(6000);
             driver.FindElements(By.TagName("a")).Where(elem => elem.GetAttribute("href") != null && elem.GetAttribute("href") == "https://controlpanel.amen.pt/domains/dns.html").First().Click();
             Thread.Sleep(5000);
@@ -74,20 +84,20 @@ namespace UpdateAmenDNSSelenium
             wait.Until(ExpectedConditions.AlertIsPresent());
             var alert = driver.SwitchTo().Alert();
             alert.Accept();
-            Console.WriteLine("Estou na pagina do dns");
+            Log("Estou na pagina do dns");
             Thread.Sleep(2000);
             driver.FindElement(By.CssSelector(".import.btn.btn-sm.btn-line-primary")).Click();
-            Console.WriteLine("Vou fazer input");
+            Log("Vou fazer input");
             var fileInput = driver.FindElement(By.Id("dnsUploadedFile"));
             fileInput.SendKeys(CreateOrUpdateCSV.dnsFile);
             driver.FindElement(By.CssSelector(".btn.btn-sm.btn-primary.apply")).Click();
-            Console.WriteLine("Submeti input");
+            Log("Submeti input");
             Thread.Sleep(6000);
             driver.FindElement(By.CssSelector(".submit.btn.btn-md.btn-primary")).Click();
             Thread.Sleep(1000);
             driver.FindElement(By.Id("modalDNS")).FindElement(By.CssSelector(".pribttn.nm.apply")).Click();
             Thread.Sleep(6000);
-            Console.WriteLine("Apliquei alterações");
+            Log("Apliquei alterações");
             Thread.Sleep(1000);
             driver.Quit();
         }
